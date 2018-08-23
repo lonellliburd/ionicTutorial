6690ebc9bf2cbd106d785df0c78987386d534c21
Lonell <lonell.liburd@gmail.com>
2018-08-23 17:31:28 -0400

    Progress

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { MenuPage } from '../menu/menu';
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
+  menuPage = MenuPage;
+
   constructor() {
 
   }
